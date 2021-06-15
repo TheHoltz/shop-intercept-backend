eac17d9014984ebb14e09e3919ebc1069eaf36c2
William Giani <holtzs.william@gmail.com>
2021-06-15 19:31:35 -0300

    Added heroku support

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ fastify.post("/", async function (request, reply) {
 });
 
 // Run the server!
-fastify.listen(3000, function (err, address) {
+fastify.listen(process.env.PORT || 3000, function (err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
